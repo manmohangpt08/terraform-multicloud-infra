d1413cf58835e05b5d2cfb6fab6790b70d430c30
Manmohan Gupta <manmohangpt08@gmail.com>
2025-10-24 19:02:47 +0530

    Fixed duplication issue

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,17 +1,13 @@
-resource "aws_db_subnet_group" "this" {
-  name       = "${var.name}-subnet-group"
-  subnet_ids = var.subnet_ids
-}
+module "rds" {
+  source             = "../../modules/rds"
+  name               = "${var.env}-db"
+  engine             = "postgres"
+  username           = "admin"
+  password           = "ChangeMe123!"
+  instance_class     = "db.t3.micro"
+  allocated_storage  = 20
+  multi_az           = false
+  subnet_ids         = module.vpc.private_subnet_ids
 
-resource "aws_db_instance" "this" {
-  identifier           = "${var.name}-db"
-  engine               = var.engine
-  instance_class       = var.instance_class
-  name                 = var.name
-  username             = var.username
-  password             = var.password
-  allocated_storage    = var.allocated_storage
-  multi_az             = var.multi_az
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.this.name
+  depends_on = [module.vpc]
 }
